c50f82fc2634f135221ae406c7cdefd44ecb36a8
xefigueroa <xefigueroa@github.com>
2021-07-23 02:34:02 +0000

    betty fix

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 
-/*
+/**
  * binary_tree_insert_right - inserts a node as the right-child of another node
  * @parent: pointer to the node to insert the right-child in
  * @value: value to store in the new node
