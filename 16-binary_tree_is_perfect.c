7b8437c5d9a23f5ebbc1218221af32e1b1f6e10c
xefigueroa <xefigueroa@github.com>
2021-07-23 02:29:58 +0000

    betty/logic fix

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -25,3 +25,23 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	return (0);
 }
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to measure the size
+ *
+ * Return: 0 If tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t l, r;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	l = binary_tree_size(tree->left);
+	r = binary_tree_size(tree->right);
+	return (1 + l + r);
+}
+
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -17,7 +17,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	}
 
 	rNew = binary_tree_node(parent, value);
-	
+
 	if (rNew == NULL)
 	{
 		return (NULL);
